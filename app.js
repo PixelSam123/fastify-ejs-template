881f27a65c8a107f9fcbe2330c8e9fa35711807c
FADHsquared <77851256+FADHsquared@users.noreply.github.com>
2021-07-27 09:16:13 +0700

    Initial commit

---
+++ app.js
@@ -0,0 +1,29 @@
+'use strict'
+
+const fastify = require('fastify')({
+  logger: true,
+})
+
+// Register your plugins here
+fastify.register(require('fastify-sensible'), {
+  errorHandler: false,
+})
+fastify.register(require('point-of-view'), {
+  engine: {
+    ejs: require('ejs'),
+  },
+  root: 'views',
+})
+
+// Autoload routes
+fastify.register(require('fastify-autoload'), {
+  dir: require('path').join(__dirname, 'routes'),
+})
+
+// Run the server!
+fastify.listen(3000, (err, address) => {
+  if (err) {
+    fastify.log.error(err)
+    process.exit(1)
+  }
+})
